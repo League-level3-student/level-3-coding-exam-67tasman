c52653633a67dfcd50ba248e9317e80163fbc49f
67tasman <arielle.sanford2@gmail.com>
2020-02-01 16:02:44 -0800

    lvl3test

---
+++ src/HashMapCalculator.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class HashMapCalculator {
+	public static void main(String args[]){
+		 HashMapCalculator c = new  HashMapCalculator();
+	}
+	
+public int commonKeyValuePairs(HashMap<String, String> h1, HashMap<String, String> h2) {
+	int r = 0;
+		if(h1.containsKey(h2.keySet())){
+			
+		}
+		
+		return r;
+	}
+
+}
--- src/VoteProcessor.java
+++ src/VoteProcessor.java
@@ -1,4 +1,48 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class VoteProcessor {
-
+public class VoteProcessor extends Level3CodingExam{
+	
+	public static void main(String args[]){
+	
+		
+	}
+	public String calculateElectionWinner(ArrayList<String> v) {
+		int pope = 0;
+		int ed = 0;
+		String res = "";
+		for(int i = 65; i < 90 ; i ++) {
+			for(int j = 0; j < v.size() ; j ++) {
+				String caps = i+"";
+				String low = (i+32) + "";
+				if(v.get(j)==caps){
+					v.get(j).equals(low);
+				}
+			}
+		}
+		
+		for(int i = 0; i < v.size() ; i ++) {
+			if(v.get(i).contains("pope")) {
+			pope++;
+			}
+			if(v.get(i).contains("edward")) {
+				ed++;
+				}
+		}
+		
+		if(ed == pope) {
+			res = "TIE";
+		}
+		if(ed < pope) {
+			res = "pope francis";
+		}
+		if(ed > pope) {
+			res = "edward snowden";
+		}
+		return res;
+		
+	}
+	
+	
+	
 }
